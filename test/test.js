e48cc44e2f036c899efbb1f5ba336028ad4e06fd
cuzz <cuzz@xqopen.cn>
2017-11-01 10:38:11 +0800

    Update test

--- test/test.js
+++ test/test.js
@@ -18,6 +18,17 @@ describe('Request', function () {
       assert.deepEqual(request3.options, {})
       assert.deepEqual(request4.options, {url: 'www.request.com'})
     })
+    it('options必须是对象', function () {
+      let request1 = new Request('www.request.com', 123)
+      let request2 = new Request('www.request.com', 'a')
+      let request3 = new Request()
+      let request4 = new Request('www.request.com', {type: 'post'})
+
+      assert.deepEqual(request1.options, {url: 'www.request.com'})
+      assert.deepEqual(request2.options, {url: 'www.request.com'})
+      assert.deepEqual(request3.options, {})
+      assert.deepEqual(request4.options, {url: 'www.request.com', type: 'post'})
+    })
   })
 
 })
