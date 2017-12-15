8a7c9320310da64f7f04b789785fe0b55ec07239
cuzz <cuzz@xqopen.cn>
2017-12-15 16:32:34 +0800

    Clean up codes

--- mock-server.js
+++ mock-server.js
@@ -1,9 +1,9 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const app = express();
 
 module.exports = {
   server: app.listen(3000),
   app: app
-}
+};
 
